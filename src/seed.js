52b3ab638465a660d7f175cb7bde99ec52158c34
noriyotcp <noriyo.akita@gmail.com>
2018-11-24 17:53:09 +0900

    Add fontawesome packages and TodoListView.vue

---
+++ src/seed.js
@@ -0,0 +1,17 @@
+export const todos = [
+  {
+    id: '1',
+    title: 'Learn Node',
+    completed: true
+  },
+  {
+    id: '2',
+    title: 'Learn React.js',
+    completed: false
+  },
+  {
+    id: '3',
+    title: 'Finished module #6',
+    completed: false
+  }
+]
